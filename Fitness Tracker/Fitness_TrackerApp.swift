394f5d95ddc0a1db573d0dd3aed74e268321a173
Ja-Urus <jgramos369@proton.me>
2023-06-26 10:39:48 -0600

    Initial Commit

---
+++ Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Jimmy Bobby on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
